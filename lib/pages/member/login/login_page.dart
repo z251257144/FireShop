65970d112a0b0b2186a9f2acce8a13c64b267896
z251257144 <zmsoul@126.com>
2019-12-13 18:03:15 +0800

    开发登录界面

--- lib/pages/member/login/login_page.dart
+++ lib/pages/member/login/login_page.dart
@@ -1,17 +1,131 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool showPassword = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text("登录"),
       ),
       body: Container(
-        
+        color: Color(0xffF0F1F2),
+        child: ListView(
+          children: <Widget>[
+            this.usernameField(),
+            Container(
+              margin: EdgeInsets.only(left: 15, right: 15),
+              color: Colors.grey,
+              height: 0.5,
+            ),
+            this.passwordField(),
+            this.loginButton()
+          ],
+        ),
       ),
     );
+  }
+
+  //用户名输入框
+  Widget usernameField() {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.fromLTRB(15, 0, 10, 0),
+      height: 52,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey, 
+            width: 0.5,
+          )
+        )
+      ),
+      child: TextField(
+        controller: this.usernameController,
+        decoration: InputDecoration(
+          icon: Image.asset("images/member/login_icon_member.png"),
+          hintText: "手机号",
+          border: InputBorder.none
+        ),
+      ),
+    );
+  }
+
+  //密码输入框
+  Widget passwordField() {
+    return Container(
+      height: 52,
+      padding: EdgeInsets.fromLTRB(15, 0, 10, 0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: 0.5,
+          )
+        )
+      ),
+      child: TextField(
+        controller: this.passwordController,
+        obscureText: !showPassword,
+        decoration: InputDecoration(
+          hintText: "密码",
+          border: InputBorder.none,
+          icon: Image.asset("images/member/login_icon_password.png"),
+          suffixIcon: this.passwordShowWidget(),
+        ),
+      ),
+    );
+  }
+
+  //密码显示控制按钮
+  Widget passwordShowWidget() {
+    return IconButton(
+      icon: this.showPassword ? Image.asset("images/member/login_icon_see.png") : Image.asset("images/member/login_icon_hide.png"),
+      onPressed: () {
+        this.setState((){
+          showPassword = !showPassword;
+        });
+        print("show password");
+      },
+    );
+  }
+
+  //登录按钮
+  Widget loginButton() {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+      height: 48,
+      child: FlatButton(
+        color: Color(0xffB4282D),
+        child: Text("登录"),
+        textColor: Colors.white,
+        onPressed: () {
+          this.doLogin();
+        },
+      ),
+    );
+  }
+
+  //登录方法
+  doLogin() {
+    print("开始登录");
+    print({"username": this.usernameController.text, "password": this.passwordController.text});
+    // print("登录结束");
   }
 }
